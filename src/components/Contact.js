2419c0a6769cbfe308c7b74b2e49db93360f7420
Tristan <you@example.com>
2024-05-24 16:52:13 +0800

    Your commit message here 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,13 +6,13 @@ const Contact = () => {
       <h4>若您對我感興趣，歡迎與我聯繫，謝謝！</h4>
       <ul>
         <li>
-         <a href="mailto:your.email@example.com"><i className="fas fa-envelope"></i> Email</a> 
+         <a href="mailto:fangdongyzu@gmail.com"><i className="fas fa-envelope"></i> Email</a> 
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/your-profile"><i className="fab fa-linkedin"></i> LinkedIn</a>
+          <a href="https://www.linkedin.com/in/fang-dong-li-b10a19217/e"><i className="fab fa-linkedin"></i> LinkedIn</a>
         </li>
         <li>
-          <a href="https://github.com/your-username"><i className="fab fa-github"></i> GitHub</a>
+          <a href="https://github.com/fangdongyzu"><i className="fab fa-github"></i> GitHub</a>
         </li>
       </ul>
     </div>
